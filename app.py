2f9387f47212addd7d5b9b978027b933866badeb
Bhargavi Bairagoni <113205911+bhargavibairagoni@users.noreply.github.com>
2024-12-23 22:43:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,5 @@ def index():
 def submit():
 	return render_template("index.html")
 if __name__ == "__main__":
-	app.run(debug=True)
-	app.run(host='0.0.0.0', port=8081) 
+	# app.run(debug=True)
+	app.run(debug=True, host='0.0.0.0', port=8081) 
